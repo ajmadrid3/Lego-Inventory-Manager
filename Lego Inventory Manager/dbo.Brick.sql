7fb1c975c2b44670e28a3a6cc0a9645de8b4412b
Andrew Madrid <amadrid118@gmail.com>
2021-09-19 19:12:24 -0500

    Added data set.

---
+++ Lego Inventory Manager/dbo.Brick.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Brick]
+(
+	[BrickElementID] INT NOT NULL PRIMARY KEY, 
+    [BrickID] INT NULL, 
+    [Name] VARCHAR(25) NULL, 
+    [TypeID] INT NULL, 
+    [ColorID] INT NULL, 
+    [Quantity] INT NULL
+)
---
+++ Lego Inventory Manager/dbo.Type.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Type]
+(
+	[TypeID] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NULL
+)
